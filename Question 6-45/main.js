62ce662ffd8e79485cc8ce97f5972c033a30b1ac
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-02 12:47:41 +0500

    first commit

---
+++ Question 6-45/main.js
@@ -0,0 +1,4 @@
+var name1 = '\n\tAhmed Raza\n\t';
+console.log(name1);
+var name2 = 'Ahmed Raza';
+console.log(name2);
---
+++ Question 6-45/main.ts
@@ -0,0 +1,4 @@
+let name1:string='\n\tAhmed Raza\n\t';
+console.log(name1);
+let name2:string='Ahmed Raza';
+console.log(name2);
