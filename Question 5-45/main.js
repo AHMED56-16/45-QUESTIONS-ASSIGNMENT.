a2f29e187be2abe471fe1eed2c133cd7b93478dc
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-02 12:17:40 +0500

    first commit

---
+++ Question 5-45/main.js
@@ -0,0 +1,2 @@
+var famaous_person = "Quaid-e-Azam Muhammad Ali Jinnah";
+console.log(famaous_person, 'once said,"I do not believe in taking the right decision, I take a decision and make it right."');
---
+++ Question 5-45/main.ts
@@ -0,0 +1,2 @@
+let famaous_person:string="Quaid-e-Azam Muhammad Ali Jinnah";
+console.log(famaous_person,'once said,"I do not believe in taking the right decision, I take a decision and make it right."');
