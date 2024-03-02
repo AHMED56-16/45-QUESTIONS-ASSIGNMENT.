5530d6578ae7a641bbe5709f74571f54938fe3eb
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-02 11:46:07 +0500

    first commit

---
+++ QUESTION 3-45/main.ts
@@ -0,0 +1,10 @@
+let personName:string='Muhammad Ahmed RAZA';
+
+console.log(personName.toUpperCase());
+
+console.log(personName.toLowerCase());
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1,19));
+console.log()
+
+
