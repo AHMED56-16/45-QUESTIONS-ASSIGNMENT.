f7e591572197c370eeddceb1101b67563ee09100
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-11 10:49:27 +0500

    first commit

---
+++ Question 10-45/Question 4-45/main.ts
@@ -0,0 +1,4 @@
+//My name is Muhammad Ahmed Raza
+//Date:11-3-2024
+//This program is about quotation of a famous person and I used quotation of Quaid-e-Azam in it. 
+console.log('Quaid-e-Azam Muhammad Ali Jinnah once said,"I do not believe in taking the right decision, I take a decision and make it right."');
---
+++ Question 10-45/Question 9-45/main.ts
@@ -0,0 +1,6 @@
+/*My name is Muhammad Ahmed Raza
+Date:11-3-2024
+This program is about my favourite number
+*/
+let favouriteNumber:number=56
+console.log('My favourite number is',favouriteNumber);
