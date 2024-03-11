d0882c485178a59a849bda319d608717a7a5fd4d
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-11 10:19:18 +0500

    first commit

---
+++ Question 7-45/main.js
@@ -0,0 +1,9 @@
+var num1 = 6;
+var num2 = 2;
+var num3 = 10;
+var num4 = 4;
+var num5 = 16;
+console.log(num1 + num2);
+console.log(num3 - num2);
+console.log(num2 * num4);
+console.log(num5 / num2);
---
+++ Question 7-45/main.ts
@@ -0,0 +1,11 @@
+let num1:number=6;
+let num2:number=2;
+let num3:number=10;
+let num4:number=4;
+let num5:number=16;
+console.log(num1+num2);
+console.log(num3-num2);
+console.log(num2*num4);
+console.log(num5/num2);
+
+
