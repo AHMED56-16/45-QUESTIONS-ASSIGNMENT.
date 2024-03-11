3e9254c5b0d936f3aa797f0fa242e570eca73ad2
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-11 11:57:32 +0500

    first commit

---
+++ Question 14-45/main.ts
@@ -0,0 +1,4 @@
+let guestList:string[]=(['Babar Azam','M Rizwan','Shahdab Khan']);
+console.log('Assalamualikum!,',guestList[0],',Hope you are fine and also in good health,I am writing to invite you to attend a dinner at my place on 11-3-24');
+console.log('Assalamualikum!,',guestList[1],',Hope you are fine and also in good health,I am writing to invite you to attend a dinner at my place on 11-3-24');
+console.log('Assalamualikum!,',guestList[2],',Hope you are fine and also in good health,I am writing to invite you to attend a dinner at my place on 11-3-24');
