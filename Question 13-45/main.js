4f32455db120a07c3836f572a8616e236d906b60
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-11 11:35:47 +0500

    first commit

---
+++ Question 13-45/main.js
@@ -0,0 +1,4 @@
+var favouriteCars = (['Honda civic', 'Audi e-tron gt', 'Lamborghini Urus']);
+console.log('I would like to own a', favouriteCars[2]);
+console.log('I would like to own a', favouriteCars[1]);
+console.log('I would like to own a', favouriteCars[0]);
---
+++ Question 13-45/main.ts
@@ -0,0 +1,4 @@
+let favouriteCars:string[]=(['Honda civic','Audi e-tron gt','Lamborghini Urus']);
+console.log('I would like to own a',favouriteCars[2]);
+console.log('I would like to own a',favouriteCars[1]);
+console.log('I would like to own a',favouriteCars[0]);
