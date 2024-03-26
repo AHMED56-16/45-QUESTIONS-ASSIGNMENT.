428fed8137dfabd6596f4240d301a921881c5fda
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-03-26 17:52:09 +0500

    first commit

---
+++ Question 20-45/main.ts
@@ -0,0 +1,5 @@
+// 19.	Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+let wc24Squad:string[]=(["BABAR AZAM(C)","M RIZWAN","SAIM AYUB","FAKHAR ZAMAN","M HARIS","IFTIKHAR AHMED","SHAHDAB KHAN","IMAD WASEEM","M NAWAZ","AAMIR JAMAL","M AMIR","SHAHEEN SHAH AFRIDI","NASEEM SHAH","HARIS RAUF","ABRAR AHMED"])
+console.log("Here is my strong 15 member squad of Pakistan cricket team for 'ICC MENS T20 WORLD CUP 2024'",wc24Squad);
+
+
