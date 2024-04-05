3be35cba11a8ba7ee1557f2b9a3eaf4652cf949b
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-05 23:58:35 +0500

    Question 23

---
+++ Question 23-45/main.js
@@ -0,0 +1,34 @@
+/*Conditional Tests: Write a series of conditional tests.
+ Print a statement describing each test and your prediction for the results of each test.
+ Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+var no1Batsman = "Babar Azam";
+console.log("Is number 1 batsman == 'Babar Azam'? I predict true");
+console.log(no1Batsman == "Babar Azam"); //Test no 1
+console.log("Is number 1 batsman == 'Shaheen Shah Afridi'? I predict false");
+console.log(no1Batsman == "Shaheen Shah Afridi"); //Test no 2
+var no1Baller = "Shaheen Shah Afridi";
+console.log("Is number 1 baller == 'Babar Azam'? I predict false");
+console.log(no1Baller == "Babar Azam"); //Test no 3
+console.log("Is number 1 baller == 'Shaheen Shah Afridi'? I predict true");
+console.log(no1Baller == "Shaheen Shah Afridi"); //Test no 4
+var no1AllRounder = "Shahdab khan";
+console.log("Is number 1 all rounder == 'Babar Azam'? I predict false");
+console.log(no1AllRounder == "Babar Azam"); //Test no 5
+console.log("Is number 1 all rounder == 'Shahdab Khan'? I predict true");
+console.log(no1AllRounder == "Shahdab Khan"); //Test no 6
+var betterCaptain = "Babar Azam";
+console.log("Is better captain for Pakistan == 'Babar Azam'? I predict true");
+console.log(betterCaptain == "Babar Azam"); //Test no 7
+console.log("Is better captain for Pakistan == 'Shaheen Shah Afridi'? I predict false");
+console.log(betterCaptain == "Shaheen Shah Afridi"); //Test no 8
+var upcomingStar = "Saim Ayub";
+console.log("Is upcoming star == 'Shahnawaz Dahani'? I predict false");
+console.log(upcomingStar == "Shahnawaz Dahani"); //Test no 9
+console.log("Is upcoming star == 'Saim Ayub'? I predict true");
+console.log(upcomingStar == "Saim Ayub"); //Test no 10
