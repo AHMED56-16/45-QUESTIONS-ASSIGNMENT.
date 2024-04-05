16e4b5d252eb81ce33c6ff9429598ea19fbfa390
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-05 18:38:07 +0500

    Question 21

---
+++ Question 21-45/main.js
@@ -0,0 +1,20 @@
+var playerNo1 = {
+    name: "Babar Azam",
+    age: 29,
+    description: "Right handed batsman and also captain of Pakistan in white ball cricket"
+};
+var playerNo2 = {
+    name: "Shahdab Khan",
+    age: 25,
+    description: "Right handed spin all-rounder and also vice captain of Pakistan in white ball cricket"
+};
+var playerNo3 = {
+    name: "M Rizwan",
+    age: 32,
+    description: "Right handed wicket keeper batsman "
+};
+var playerNo4 = {
+    name: "Shaheen Shah Afridi",
+    age: 23,
+    description: "Left arm pacer and leading bowler of pakistan cricket team"
+};
---
+++ Question 21-45/main.ts
@@ -0,0 +1,27 @@
+// 	They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+interface cricketers {
+    name:string
+    age:number
+    description:string
+}
+let player1 :cricketers={
+    name:"Babar Azam",
+    age:29,
+    description:"Right handed batsman and also captain of Pakistan in white ball cricket"
+}
+let  player2 :cricketers={
+    name:"Shahdab Khan",
+    age:25,
+    description:"Right handed spin all-rounder and also vice captain of Pakistan in white ball cricket"
+}
+let  player3 :cricketers={
+    name:"M Rizwan",
+    age:32,
+    description:"Right handed wicket keeper batsman "
+}
+let  player4 :cricketers={
+    name:"Shaheen Shah Afridi",
+    age:23,
+    description:"Left arm pacer and leading bowler of pakistan cricket team"
+}
+
