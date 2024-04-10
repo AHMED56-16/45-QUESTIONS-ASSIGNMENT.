38e3589038e6fb6ecc45c75837d4403a675a8ad9
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-10 18:17:01 +0500

    Question 25

---
+++ Question 25-45/main.ts
@@ -0,0 +1,17 @@
+let alienColor: string = "green";
+console.log('Test 1');
+//If it passes the test print a message that the player just earned 5 points.
+if (alienColor=="green"){
+    console.log("The player just earned 5 points.");
+}  
+else{
+    console.log("");
+}  
+console.log('Test 2');  //Failed version
+//The version that fails will have no output       
+if (alienColor=="yellow"){
+    console.log("The player just earned 5 points.");
+}  
+else{
+    console.log("");
+}                     
