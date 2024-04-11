ea6a47ef3b9a07aa9af9e0beeab60db34bf3b233
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-11 20:10:01 +0500

    Question 26

---
+++ Question 26-45/main.js
@@ -0,0 +1,16 @@
+var alienColor = "green";
+console.log('Test 1');
+//If it passes the test print a message that the player just earned 5 points.
+if (alienColor == "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("You just earned 10 points");
+}
+console.log('Test 2');
+if (alienColor == "yellow") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("You just earned 10 points");
+}
