e46aadb35f7c330cd606d50a47aeb52a2ad46056
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-11 20:19:42 +0500

    Question 27

---
+++ Question 27-45/main.ts
@@ -0,0 +1,27 @@
+let alienColor: string = "green";
+console.log('Test 1');
+if (alienColor == "green") {
+    console.log("You earned 5 points.");
+  } else if (alienColor == "yellow") {
+    console.log("You earned 10 points.");
+  } else if (alienColor == "red") {
+    console.log("You earned 15 points.");
+  }
+console.log('Test 2');
+alienColor = "yellow";
+if (alienColor == "green") {
+  console.log("You earned 5 points.");
+} else if (alienColor == "yellow") {
+  console.log("You earned 10 points.");
+} else if (alienColor == "red") {
+  console.log("You earned 15 points.");
+}
+console.log('Test 3');
+alienColor = "red";
+if (alienColor == "green") {
+  console.log("You earned 5 points.");
+} else if (alienColor == "yellow") {
+  console.log("You earned 10 points.");
+} else if (alienColor == "red") {
+  console.log("You earned 15 points.");
+}
