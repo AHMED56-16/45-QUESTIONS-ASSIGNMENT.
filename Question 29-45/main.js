3514e7afd4dccf4645454031e2dee52b1963fa95
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-12 18:02:07 +0500

    Question 29

---
+++ Question 29-45/main.js
@@ -0,0 +1,16 @@
+var favorite_fruits = ["watermelon", "orange", "mango"];
+if (favorite_fruits.includes("watermelon")) {
+    console.log("You really like watermelon!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like orange!");
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango!");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apple!");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana!");
+}
---
+++ Question 29-45/main.ts
@@ -0,0 +1,17 @@
+let favorite_fruits: string[] = ["watermelon", "orange", "mango"];
+
+if (favorite_fruits.includes("watermelon")) {
+  console.log("You really like watermelon!");
+}
+if (favorite_fruits.includes("orange")) {
+  console.log("You really like orange!");
+}
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango!");
+  }
+  if (favorite_fruits.includes("apple")) {
+    console.log("You really like apple!");
+  }
+  if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana!");
+  }
