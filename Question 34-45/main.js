7d55f84311ddf2b193e9b1ac2315516ce4381068
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-13 11:04:52 +0500

    Question 34

---
+++ Question 34-45/main.js
@@ -0,0 +1,5 @@
+var pizzaFlavors = ['Cheese and Pepperoni', "Cheese and Barbeque Chicken", " Double Cheese Pizza"];
+pizzaFlavors.forEach(function (flavor) {
+    console.log("I like ".concat(flavor, "pizza,"));
+});
+console.log("I like pizza and my favorite flavors are Cheese and Pepperoni,Cheese and Barbeque Chicken, Double Cheese Pizza.I really love pizza!");
---
+++ Question 34-45/main.ts
@@ -0,0 +1,7 @@
+let pizzaFlavors:string[]=['Cheese and Pepperoni',"Cheese and Barbeque Chicken"," Double Cheese Pizza"]
+
+pizzaFlavors.forEach(flavor => {
+    console.log(`I like ${flavor}pizza,`)
+});
+console.log("I like pizza and my favorite flavors are Cheese and Pepperoni,Cheese and Barbeque Chicken, Double Cheese Pizza.I really love pizza!"
+)
