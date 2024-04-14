d51689b8e37e78f4ebaa52440c45a2cd6cea3d15
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-14 22:25:48 +0500

    Q 39

---
+++ Question 39-45/main.js
@@ -0,0 +1,6 @@
+function describeCity(city, country) {
+    return "".concat(city, " ,").concat(country);
+}
+console.log(describeCity("Karachi", "Pakistan"));
+console.log(describeCity("Dubai", "United Arab Emirates"));
+console.log(describeCity("Mumbai", "India"));
---
+++ Question 39-45/main.ts
@@ -0,0 +1,7 @@
+function describeCity(city: string,country: string): string {
+    return `${city} ,${country}`
+}
+
+console.log(describeCity("Karachi","Pakistan"))
+console.log(describeCity("Dubai","United Arab Emirates"))
+console.log(describeCity("Mumbai","India"))
