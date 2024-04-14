90509f508dbeeb11b5f008106c5615d7eb84d14b
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-14 18:17:39 +0500

    Q 35

---
+++ Question 35-45/main.js
@@ -0,0 +1,5 @@
+var wildAnimals = ["lion", "tiger", "leopard"];
+wildAnimals.forEach(function (animal) {
+    console.log("A ".concat(animal, " is a wild animal"));
+});
+console.log("Lions, tigers, and leopards are all big cats belonging to the Panthera genus. They share several characteristics in common:Carnivorous Diet,Powerful Predators,Territorial Behavior and etc.");
---
+++ Question 35-45/main.ts
@@ -0,0 +1,9 @@
+let wildAnimals:string[] = ["lion", "tiger", "leopard"];
+
+wildAnimals.forEach(
+    (animal:string) => {
+        console.log(`A ${animal} is a wild animal`);
+    }
+)
+console.log("Lions, tigers, and leopards are all big cats belonging to the Panthera genus. They share several characteristics in common:Carnivorous Diet,Powerful Predators,Territorial Behavior and etc.")
+
