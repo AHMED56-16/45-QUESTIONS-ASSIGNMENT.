8186f88fe2f3b25e78b49d0ae01bc6f519f87312
AHMED56-16 <161157786+AHMED56-16@users.noreply.github.com>
2024-04-14 21:44:39 +0500

    Q 36

---
+++ Question 36-45/main.js
@@ -0,0 +1,4 @@
+function makeShirt(size, message) {
+    console.log("My brand's most selling t-shirt is of ".concat(size, " size  with the message \"").concat(message, "\" printed on it."));
+}
+makeShirt("small", "'Free Palestine'");
---
+++ Question 36-45/main.ts
@@ -0,0 +1,4 @@
+function makeShirt(size: string ,message: string) {
+    console.log(`My brand's most selling t-shirt is of ${size} size  with the message "${message}" printed on it.`);
+}
+makeShirt("small","'Free Palestine'")
